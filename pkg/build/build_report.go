ac9b4c435293b2e534a49c6b0cc499bd7950e2e3
Aleksei Igrychev <aleksei.igrychev@palark.com>
2026-04-30 23:21:45 +0100

    refactor(build): consolidate GetContextDependencies API and Conveyor interface
    Signed-off-by: Aleksei Igrychev <aleksei.igrychev@palark.com>

--- pkg/build/build_report.go
+++ pkg/build/build_report.go
@@ -213,20 +213,8 @@ func createBuildReport(ctx context.Context, phase *BuildPhase, imagePairs []util
 		targetPlatforms := util.MapFuncToSlice(images, func(img *image.Image) string { return img.TargetPlatform })
 
 		for _, img := range images {
-			imageDesc := img.GetFinalContextTagDesc()
-			if imageDesc == nil {
-				imageDesc = img.GetContextTagDesc()
-			}
-			if imageDesc == nil {
-				stageImage := img.GetLastNonEmptyStage().GetStageImage().Image
-				imageDesc = stageImage.GetFinalStageDesc()
-				if imageDesc == nil {
-					imageDesc = stageImage.GetStageDesc()
-				}
-			}
-
+			imageDesc := img.GetContextTagDesc()
 			stages := getStagesReport(img, false)
-
 			configType := determineConfigType(phase.Conveyor.werfConfig, img.Name)
 
 			record := ReportImageRecord{
@@ -241,7 +229,7 @@ func createBuildReport(ctx context.Context, phase *BuildPhase, imagePairs []util
 				Final:             img.IsFinal,
 				Size:              imageDesc.Info.Size,
 				BuildTime:         fmt.Sprintf("%.2f", img.BuildDuration.Seconds()),
-				Commit:            stageDesc.Info.Labels[imagePkg.WerfProjectRepoCommitLabel],
+				Commit:            imageDesc.Info.Labels[imagePkg.WerfProjectRepoCommitLabel],
 				Stages:            stages,
 				ConfigType:        configType,
 			}
